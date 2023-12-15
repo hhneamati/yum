46021d025416be0b37bb0790305221fbf5be4592
hhneamati <hhneamati@gmail.com>
2023-12-15 19:27:41 +0330

    debug user

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser , PermissionsMixin
 
 
-class CustomUser(AbstractUser , PermissionsMixin):
-    username = None
+class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=11, unique=True)
     full_name = models.CharField(max_length=100)
     job = models.CharField(max_length=100, blank=True, null=True)
